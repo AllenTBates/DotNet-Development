959113c4b14c4b82633f86b02a01475336ee7012
AllenTBates <gamecockazul@gmail.com>
2018-03-21 16:57:40 -0400

    Add project files.

---
+++ Movie/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Movie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public decimal TicketPrice { get; set; }
+        public string Genre { get; set; }
+
+        // Foreign Key
+        public int DirectorId { get; set; }
+        // Navigation property
+        public Director Director { get; set; }
+    }
+}
---
+++ MovieService/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieService.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+
+        // Foreign Key
+        public int DirectorId { get; set; }
+        // Navigation property
+        public Director Director { get; set; }
+    }
+}
